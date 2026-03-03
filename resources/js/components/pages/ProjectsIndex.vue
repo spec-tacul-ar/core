e9401f7a2df651b9ae13d35d91339e7610c1aa5e
Matthew White <matt@matthewwhite.me.uk>
2026-03-03 20:51:16 +0000

    Added an empty state for the projects page.

--- resources/js/components/pages/ProjectsIndex.vue
+++ resources/js/components/pages/ProjectsIndex.vue
@@ -4,14 +4,25 @@
             <div class="flex justify-between flex-wrap items-end gap-4 mb-4 sm:pl-4">
                 <h1 class="font-display font-semibold text-4xl">Projects</h1>
                 
-                <RouterLink :to="{ name: 'projects.create' }" class="btn btn-primary">Create project</RouterLink>
+                <RouterLink v-if="!is_loading_projects && projects.isNotEmpty()" :to="{ name: 'projects.create' }" class="btn btn-primary">Create project</RouterLink>
             </div>
 
             <LoadingSpinner label="Loading projects" v-if="is_loading_projects" />
 
-            <!-- TODO: CTA for creating a new project if none exist -->
+            <div
+                v-if="!is_loading_projects && projects.isEmpty()"
+                class="flex flex-col items-center gap-2 bg-white/25 border-2 border-gray-200 border-dashed rounded-2xl p-4">
 
-            <Card class="divide-y divide-gray-300">
+                <IconSet name="create-project" class="size-16 text-gray-600" />
+
+                <h2 class="font-semibold">Welcome to Spectacular!</h2>
+
+                <p>Get started by creating your first project</p>
+
+                <RouterLink :to="{ name: 'projects.create' }" class="btn btn-primary">Create project</RouterLink>
+            </div>
+
+            <Card v-if="projects.isNotEmpty()" class="divide-y divide-gray-300">
                 <ProjectItem v-for="project in projects" :key="project.id" :project="project" />
             </Card>
         </section>
