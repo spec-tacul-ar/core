42d84a505aac5468a8ab218a20a45c88b3f5bc6e
Matthew White <matt@matthewwhite.me.uk>
2026-03-29 21:51:05 +0100

    Forcing HTTPS URLs. Removed unused dependencies. Updated dependencies.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ use App\Models\Feature;
 use App\Models\Requirement;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        URL::forceHttps(app()->environment(['production', 'staging']));
+
         Relation::enforceMorphMap([
             'account' => Account::class,
             'feature' => Feature::class,
