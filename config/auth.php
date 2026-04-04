fbdb3fe47477f9a059b9dd20328e7e1e5255ac67
Matthew White <matt@matthewwhite.me.uk>
2026-04-04 12:04:37 +0100

    Fixed password reset.

--- config/auth.php
+++ config/auth.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Models\Account;
-
 return [
 
     /*
@@ -17,7 +15,7 @@ return [
 
     'defaults' => [
         'guard' => env('AUTH_GUARD', 'web'),
-        'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
+        'passwords' => env('AUTH_PASSWORD_BROKER', 'accounts'),
     ],
 
     /*
@@ -40,7 +38,7 @@ return [
     'guards' => [
         'web' => [
             'driver' => 'session',
-            'provider' => 'users',
+            'provider' => 'accounts',
         ],
     ],
 
@@ -62,9 +60,9 @@ return [
     */
 
     'providers' => [
-        'users' => [
+        'accounts' => [
             'driver' => 'eloquent',
-            'model' => env('AUTH_MODEL', Account::class),
+            'model' => env('AUTH_MODEL', App\Models\Account::class),
         ],
 
         // 'users' => [
@@ -93,8 +91,8 @@ return [
     */
 
     'passwords' => [
-        'users' => [
-            'provider' => 'users',
+        'accounts' => [
+            'provider' => 'accounts',
             'table' => env('AUTH_PASSWORD_RESET_TOKEN_TABLE', 'password_reset_tokens'),
             'expire' => 60,
             'throttle' => 60,
