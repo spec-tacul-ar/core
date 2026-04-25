d6ea126797382d0a9b4b9b9fdf15dc8d49731b90
Matthew White <matt@matthewwhite.me.uk>
2026-04-25 10:13:07 +0100

    Better exception for the importer.

--- app/Exceptions/ProjectAlreadyExistsException.php
+++ app/Exceptions/ProjectAlreadyExistsException.php
@@ -3,12 +3,22 @@
 namespace App\Exceptions;
 
 use App\Models\Project;
+use Illuminate\Contracts\Debug\ShouldntReport;
+use Illuminate\Http\Request;
 use RuntimeException;
+use Symfony\Component\HttpFoundation\Response;
 
-class ProjectAlreadyExistsException extends RuntimeException
+class ProjectAlreadyExistsException extends RuntimeException implements ShouldntReport
 {
     public function __construct(public Project $project)
     {
         parent::__construct('A project with this UUID already exists.');
+    }
+
+    public function render(Request $request): Response
+    {
+        return response()->json([
+            'message' => $this->getMessage(),
+        ], 409);
     }
 }
--- tests/Unit/ProjectAlreadyExistsExceptionTest.php
+++ tests/Unit/ProjectAlreadyExistsExceptionTest.php
@@ -4,6 +4,9 @@ namespace Tests\Unit;
 
 use App\Exceptions\ProjectAlreadyExistsException;
 use App\Models\Project;
+use Illuminate\Contracts\Debug\ShouldntReport;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Tests\TestCase;
 
 class ProjectAlreadyExistsExceptionTest extends TestCase
@@ -18,5 +21,32 @@ class ProjectAlreadyExistsExceptionTest extends TestCase
 
         $this->assertSame($project, $exception->project);
         $this->assertSame('A project with this UUID already exists.', $exception->getMessage());
+    }
+
+    public function test_it_is_not_reported(): void
+    {
+        $exception = new ProjectAlreadyExistsException(new Project([
+            'name' => 'Existing Project',
+        ]));
+
+        $this->assertInstanceOf(ShouldntReport::class, $exception);
+    }
+
+    public function test_it_renders_as_a_basic_json_message_for_json_requests(): void
+    {
+        $project = new Project([
+            'name' => 'Existing Project',
+        ]);
+
+        $exception = new ProjectAlreadyExistsException($project);
+        $request = Request::create('/api/projects/demo', 'POST', server: [
+            'HTTP_ACCEPT' => 'application/json',
+        ]);
+
+        $response = $exception->render($request);
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertSame(409, $response->getStatusCode());
+        $this->assertSame('A project with this UUID already exists.', $response->getData(true)['message']);
     }
 }
